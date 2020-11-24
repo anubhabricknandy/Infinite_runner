89bf2b7fdc7c8c5b1e21974c88fc5e8f1ca57264
unknown <anubhabnandy1998@gmail.com>
2020-11-24 08:29:15 +0530

    first

---
+++ College Project Infinite Runner/Assets/Scripts/deathMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class deathMenu : MonoBehaviour {
+    public Text jj;
+	// Use this for initialization
+	void Start () {
+        //  gameObject.SetActive(false);
+        jj.text = PlayerPrefs.GetString("xxx");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void deathScore(string Finalscore)
+    {
+        jj.text = Finalscore;
+       // gameObject.SetActive(true);
+    }
+}
---
+++ College Project Infinite Runner/Assets/Scripts/playerMoves.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class playerMoves : MonoBehaviour {
+
+    private CharacterController controller;
+    private float speed = 5.0f;
+    private Vector3 moveVector;
+    private float gravity = 12.0f;
+
+    public scoreBoard sb;
+
+    private float verticalVelocity = 0.0f;
+    public Text gg;
+    public deathMenu dm;
+    // Use this for initialization
+    void Start () {
+        controller = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+       moveVector = Vector3.zero;
+        if(controller.isGrounded)
+        {
+            verticalVelocity = -0.5f;
+        }
+        else
+        {
+            verticalVelocity -= gravity* Time.deltaTime;
+        }
+
+        moveVector.x = Input.GetAxisRaw("Horizontal") * speed;
+        moveVector.y = verticalVelocity;
+        moveVector.z = speed;
+
+        controller.Move(moveVector * Time.deltaTime);
+	}
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag=="Enemy")
+        {
+            string str = sb.scoreText.text;
+              Debug.Log(str);
+            PlayerPrefs.SetString("xxx", str);
+         //   dm.deathScore(str);
+         //   Time.timeScale = 0;
+           
+            SceneManager.LoadScene("Game Over");
+        }
+    }
+    public void setSpeed(float modifier)
+    {
+        speed = 5.0f + modifier;
+    }
+    
+}
